e71fd0e2411bf13a5a59676c97c1049dddf6424c
Michael Angerman <stormasm@gmail.com>
2017-08-09 09:29:37 -0700

    syncing up with shopping-cart-eject, but still not working

--- scripts/server.js
+++ scripts/server.js
@@ -1,7 +1,11 @@
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
 var webpack = require('webpack');
 var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
-var config = require('./webpack.config');
+var config = require('./../config/webpack.config.dev');
 
 var app = new require('express')();
 var port = 3000;
@@ -9,9 +13,12 @@ var port = 3000;
 var compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
 app.use(webpackHotMiddleware(compiler));
+/*
+This is no longer needed with this new implementation...
 app.use(function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
+*/
 
 app.listen(port, function(error) {
   if (error) {
